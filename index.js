27f2568e3914facb80e019f9a47b13ecebd0df7f
Christopher Hiller <boneskull@boneskull.com>
2017-08-06 01:19:39 -0700

    seems to work ok

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const HT16K33_BLINK_FREQS = new Set([
   0x00,
   0x02,
   0x04,
-  0x06
+  0x06,
+  false
 ]);
 const HT16K33_SYSTEM_SETUP = 0x20;
 const HT16K33_SYSTEM_SETUP_NORMAL = 0x01;
@@ -21,6 +22,7 @@ const HT16K33_BRIGHTNESS = 0xE0;
 const DEFAULT_BRIGHTNESS = 100;
 const DEFAULT_ADDRESS = HT16K33_ADDRESS;
 const DEFAULT_COLS = 24;
+const DEFAULT_DEVICES = 1;
 
 const LED_OFF = 0;
 const LED_GREEN = 1;
@@ -33,14 +35,17 @@ class BicolorBargraph {
   }
 
   static mapBrightness (value) {
-    return Board.map(val, 0, 100, 0, 15);
+    return Board.map(value, 0, 100, 0, 15);
   }
 
   constructor (opts = {}) {
     opts = defaults(opts, {
       addresses: [DEFAULT_ADDRESS],
       brightness: DEFAULT_BRIGHTNESS,
-      columns: DEFAULT_COLS
+      columns: DEFAULT_COLS,
+      devices: !isUndefined(opts.addresses) && opts.addresses.length
+        ? opts.addresses.length
+        : DEFAULT_DEVICES
     })
     Board.Component.call(this, opts = Board.Options(opts));
 
@@ -49,67 +54,73 @@ class BicolorBargraph {
       Array(16)
         .fill(0)
     ];
-    this.columns = opts.columns;
+
+    Object.assign(this, {
+      columns: opts.columns,
+      addresses: opts.addresses,
+      devices: opts.devices
+    });
+
     this.each(function (addr) {
       this.on(addr);
       this.blink(addr, false);
-      this.brightness(opts.brightness);
+      this.brightness(addr, opts.brightness);
       this.clear(addr);
     });
   }
 
   on (addr) {
     if (isUndefined(addr)) {
-      this.each(function (addr) {
+      return this.each(function (addr) {
         this.on(addr);
       });
-    } else {
-      this._send(addr, HT16K33_SYSTEM_SETUP, HT16K33_SYSTEM_SETUP_NORMAL);
     }
-    return this;
+    return this._send(addr, HT16K33_SYSTEM_SETUP, HT16K33_SYSTEM_SETUP_NORMAL);
   }
 
   off (addr) {
     if (isUndefined(addr)) {
-      this.each(function (addr) {
+      return this.each(function (addr) {
         this.off(addr);
       });
-    } else {
-      this._send(addr, HT16K33_SYSTEM_SETUP, HT16K33_SYSTEM_SETUP_STANDBY);
     }
-    return this;
+    return this._send(addr, HT16K33_SYSTEM_SETUP, HT16K33_SYSTEM_SETUP_STANDBY);
   }
 
   brightness (addr, value = 100) {
     if (isUndefined(addr)) {
-      this.each(function (addr) {
+      return this.each(function (addr) {
         this.brightness(addr, value);
       });
-    } else {
-      this._send(addr, HT16K33_BRIGHTNESS,
-        BicolorBargraph.mapBrightness(value));
     }
-    return this;
+    return this._send(addr, HT16K33_BRIGHTNESS,
+      BicolorBargraph.mapBrightness(value));
+  }
+
+  _led (addr, col, value) {
+    const pos = Math.floor(col / 8);
+    const offset = col % 8;
+    this.buffer[addr][pos] = !value
+      ? this.buffer[addr][pos] & ~(1 << offset)
+      : this.buffer[addr][pos] | (1 << offset);
+    return this._writeDisplay(addr);
   }
 
   led (addr, col, value) {
     if (isUndefined(value)) {
       value = col;
       col = addr;
-      this.each(function (addr) {
+      return this.each(function (addr) {
         this.led(addr, col, value);
       });
-    } else {
-      if (col >= this.columns) {
-        throw new Error(`column ${col} out of range (max ${this.columns})`);
-      }
-      const pos = Math.floor(col / 8);
-      const offset = col % 8;
-      this.buffer[addr][pos] = value === 0
-        ? this.buffer[addr][pos] & ~(1 << offset)
-        : this.buffer[addr][pos] | (1 << offset);
-      this._writeDisplay(addr);
     }
+    if (col >= this.columns) {
+      throw new Error(`column ${col} out of range (max ${this.columns})`);
+    }
+    const cathode = Math.floor((col < 12 ? col : col - 12) / 4);
+    const anode = col >= 12 ? col % 4 + 4 : col % 4;
+    this._led(addr, cathode * 16 + anode + 8, value & LED_GREEN ? 1 : 0);
+    this._led(addr, cathode * 16 + anode, value & LED_RED ? 1 : 0);
     return this;
   }
 
@@ -117,51 +128,53 @@ class BicolorBargraph {
     const buf = this.buffer[addr];
     const bytes = [0x00];
     // always writes 8 rows (for 8x16, the values have already been rotated)
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < buf.length; i++) {
       bytes.push(buf[i] & 0xFF);
-      bytes.push(buf[i] >> 8);
     }
     this.io.i2cWrite(this.addresses[addr], bytes);
+    return this;
   }
 
   clear (addr) {
     if (isUndefined(addr)) {
-      this.each(function (addr) {
+      return this.each(function (addr) {
         this.clear(addr);
       });
-    } else {
-      this.buffer[addr] = Array(16)
-        .fill(0);
-      this._writeDisplay(addr);
     }
-    return this;
+    this.buffer[addr] = Array(16)
+      .fill(0);
+    return this._writeDisplay(addr);
   }
 
   blink (addr, freq) {
     if (isUndefined(freq)) {
       freq = addr;
-      this.each(function (addr) {
+      return this.each(function (addr) {
         this.blink(addr, freq);
       });
-    } else {
-      if (!HT16K33_BLINK_FREQS.has(freq)) {
-        throw new Error(`invalid frequency; should be one of ${Array.from(
-          HT16K33_BLINK_FREQS)}`);
-      }
-      this._send(addr, HT16K33_BLINK, HTK33_BLINK_ON | freq);
     }
-    return this;
+    if (!HT16K33_BLINK_FREQS.has(freq)) {
+      throw new Error(`invalid frequency; should be one of ${Array.from(
+        HT16K33_BLINK_FREQS)}`);
+    }
+    return this._send(addr, HT16K33_BLINK, HT16K33_BLINK_ON | freq);
   }
 
   _send (addr, opcode, data) {
     if (isUndefined(data)) {
       data = opcode;
       opcode = addr;
-      this.each(function (addr) {
+      return this.each(function (addr) {
         this._send(addr, opcode, data);
       });
-    } else {
-      this.io.i2cWrite(this.addresses[addr], [opcode | data]);
+    }
+    this.io.i2cWrite(this.addresses[addr], [opcode | data]);
+    return this;
+  }
+
+  each (callback) {
+    for (let i = 0; i < this.devices; i++) {
+      callback.call(this, i);
     }
     return this;
   }
